8ffa619c42231a26ad5a8c025e26060899908a31
lizonglin313 <lizonglin313@outlook.com>
2020-07-20 18:56:26 +0800

    增加删除文章

--- src/test/NAAASTest.java
+++ src/test/NAAASTest.java
@@ -19,13 +19,23 @@ public class NAAASTest {
     @Test
     public void testCreateArticle() {
         //String title, String date, String context, String cname
-        String title = "雷霆八嘎";
+        String title = "窝窝头";
         String date = "2020-07-18";
-        String context = "小鸟伏特加喝了这么高";
+        String context = "小鸟伏特加";
         String cname = "时尚";
 
         NewAndAlterArticleService newAndAlterArticleService = new NewAndAlterArticleService();
         newAndAlterArticleService.createArticle(title,date,context,cname);
+    }
+
+    // 测试删除文章
+    @Test
+    public void deleteArticle(){
+        int article_id = 38;
+        int row;
+        NewAndAlterArticleService newAndAlterArticleService = new NewAndAlterArticleService();
+        row = newAndAlterArticleService.delete_Article(article_id);
+        System.out.print(row);
     }
 
 }
